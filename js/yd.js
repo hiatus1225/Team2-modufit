0e94e11dae337f73196b57a5c227361c01fdb3ac
yyd3157 <70931998+yyd3157@users.noreply.github.com>
2021-08-25 20:45:20 +0900

    유영대
    테스트 수정 3차

--- js/yd.js
+++ js/yd.js
@@ -1,12 +1,11 @@
 function categoryChange(e) {
-    var d;
-    var target = document.getElementById("name");
-    var gender = document.getElementById('gender').value;
-    var name_men = ["홍길동","어린왕자","심봉사"];
-    var name_women = ["춘향", "콩쥐", "팥쥐", "신데렐라"];
-    
-        if(e.value == "men") d = name_men;
-        else if(e.value == "women") d = name_women;
+	var name_men = ["홍길동","어린왕자","심봉사"];
+	var name_women = ["춘향", "콩쥐", "팥쥐", "신데렐라"];
+	var target = document.getElementById("name");
+
+	if(e.value == "men") var d = name_men;
+	else if(e.value == "women") var d = name_women;
+	
     target.options.length = 0;
 
 	for (x in d) {
@@ -18,13 +17,15 @@ function categoryChange(e) {
 }
 
 function button01_click(){
-
+    
     var date = document.getElementById('date').value;
     var gender = document.getElementById('gender').value;
     var name = document.getElementById('name').value;
     var time = document.getElementById('time').value;
-
-    var count = localStorage.length+1;
+    if((date === "")||(gender === "")||(name === "")||(time === "")){
+        alert('정보를 모두 입력해주세요');} 
+        else{
+    var count = localStorage.length;
     console.log(count);
 
     var applykey = '__apply__' + count;
@@ -33,10 +34,12 @@ function button01_click(){
         gender: gender,
         name: name,
         time: time
-    };
+    }
+    
+    
     console.log(userInfo);
     localStorage.setItem(applykey, JSON.stringify(userInfo));
-
+    }
 }
 
 localStorage.setItem(document.getElementById('date').value, document.getElementById('date').value);
