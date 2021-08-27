c986808c4fc8d89fac30d5d32b530c325a24d58f
nueey <89372098+nueey@users.noreply.github.com>
2021-08-27 10:29:59 +0900

    문예은
    join js파일 이동

--- js/join.js
+++ js/join.js
@@ -1,33 +1,143 @@
-function button1_click(){
+
+  function idcheck_click() {
+
+    var key = Object.keys(localStorage);
+    var localID= document.getElementById("id").value;
+    if(localID===null || localID===""){
+      document.getElementById("id").style.backgroundColor="white";
+    }
+    if(localID===null || localID===""){
+      alert("아이디를 입력해 주세요.");
+      return;
+    }
+    for(let i = 0; i < key.length;i++){
+      if (key[i]!=="__user__"){
+        if("__user__"+localID === key[i]){
+            // alert("이미 가입된 아이디가 존재합니다.");
+          document.getElementById("id").style.backgroundColor="red";
+          style="background-color: rgb(162, 255, 162);"
+            return;
+          }else {
+            document.getElementById("id").style.backgroundColor="rgb(162, 255, 162)";
+          // document.getElementById("idcheck").value="확인완료";
+        }
+      }
+    }
+  }
+
+  function pwcheck_click(){
+
+    var pw1 = document.getElementById('pw1').value;
+    var pw2 = document.getElementById('pw2').value;
+    if(pw1===null || pw1===""){document.getElementById("pw1").style.backgroundColor="white";}
+    if(pw2===null || pw2===""){document.getElementById("pw2").style.backgroundColor="white";}
+
+    if(pw1 != pw2){
+      // alert("비밀번호가 일치하지 않습니다.");
+      document.getElementById("pw1").style.backgroundColor="red";
+      document.getElementById("pw2").style.backgroundColor="red";
+      document.f2.pw2=null;
+      return false;
+    } else{
+      // alert("비밀번호가 일치합니다.");
+      document.getElementById("pw1").style.backgroundColor="rgb(162, 255, 162)";
+      document.getElementById("pw2").style.backgroundColor="rgb(162, 255, 162)";
+      return true;
+    }
+  }
+
+
+
+  function button1_click(){
 
     var name = document.getElementById('name').value;
-    // localStorage.setItem("name", ui1)
+   
     var contact = document.getElementById('contact').value;
-    // localStorage.setItem("contact", ui2)
+  
     var id = document.getElementById('id').value;
-    // localStorage.setItem("user", ui3)
-    var pw = document.getElementById('pw').value;
-    // localStorage.setItem("pw", ui4)
-    var age = document.getElementById('age').value;
-    // localStorage.setItem("age", ui5)
-    var gender = document.getElementById('gender').value;
-    // localStorage.setItem("gender", ui6)
+   
+    var pw1 = document.getElementById('pw1').value;
+   
+    var pw2 = document.getElementById('pw2').value;
 
-    // console.log(ui, ui2, ui3, ui4, ui5, ui6);
+    var age = document.getElementById('age').value;
+    
+    var gender = document.getElementById('gender').value;
+    
+    var address = document.getElementById('address').value;
+   
+
     var userkey = '__user__' + id;
     var userInfo = {
       name: name,
       contact: contact,
       id: id,
-      pw: pw,
+      pw1: pw1,
       age: age,
       gender: gender,
       rank:1
-    };
+    }
+
+    if(id==""){
+        alert("아이디를 입력해주세요.");
+        document.f2.id.focus();
+        return;
+    }
+
+    if(pw1==""){
+        alert("비밀번호를 입력해주세요.");
+        document.f2.pw1.focus();
+        return;
+    }
+
+    if(pw2==""){
+        alert("비밀번호를 확인해주세요.");
+        document.f2.pw2.focus();
+        return;
+    }
+
+    if(name==""){
+        alert("성명을 입력해주세요.");
+        document.f2.name.focus();
+        return;
+    }
+
+    if(contact==""){
+        alert("연락처를 입력해주세요.");
+        document.f2.contact.focus();
+        return;
+    }
+
+    if(age==""){
+        alert("나이를 입력해주세요.");
+        document.f2.age.focus();
+        return;
+    }
+
+    if(age < 14 || age > 124){
+        alert("정상적인 나이를 입력해주세요.");
+        document.f2.age.focus();
+        return;
+    }
+
+    if(address==""){
+        alert("주소를 입력해주세요.");
+        document.f2.address.focus();
+        return;
+    }
+    
+
+    
+    //저장값을 한줄로
     console.log(userInfo);
     localStorage.setItem(userkey, JSON.stringify(userInfo));
+
     // -----
     location.href="login.html";
+
   }
-  localStorage.setItem(document.getElementById('name').value, document.getElementById('name').value);
   
+    localStorage.setItem(document.getElementById('name').value, document.getElementById('name').value);
+        
+      
+      
